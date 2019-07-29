59cec11601f1b77235f5a353919bb5968f123755
Josh Dab <josiah.dabuet@gmail.com>
2019-07-29 15:35:38 +0800

    created navbar (without logo) and featured articles

--- wp/wp-content/themes/prstation/index.php
+++ wp/wp-content/themes/prstation/index.php
@@ -16,8 +16,71 @@
 
 get_header();
 ?>
+<?php get_template_part( 'template-parts/header/main'); ?>
 
-	AND SO IT BEGINS...
+<main class="l-index">
+  <div class="l-index__inner l-container">
+    <div class="l-index__content">
+      <div class="featured">
+        <ul class="featured__list">
+          <li class="featured__list-item">
+            <a class="featured__list-item-link" href="#">
+              <article class="article-block">
+                <div class="article-block__top">
+                  <span class="article-block__category">Apps</span>
+                  <div class="article-block__image" style="background-image: url('https://placeimg.com/350/219/nature');"></div>
+                </div>
+                <time class="article-block__date" datetime="07-11-19">JUL 11, 2019</time>
+                <h3 class="article-block__title">GrabShare Launches New Enhanced Option for Better Matched Rides</h3>
+                <p class="article-block__author">Grab Holdings Inc.</p>
+              </article>
+            </a>
+          </li>
+          <li class="featured__list-item">
+            <a class="featured__list-item-link" href="#">
+              <article class="article-block">
+                <div class="article-block__top">
+                  <span class="article-block__category">Apps</span>
+                  <div class="article-block__image" style="background-image: url('https://placeimg.com/350/219/nature');"></div>
+                </div>
+                <time class="article-block__date" datetime="07-11-19">JUL 11, 2019</time>
+                <h3 class="article-block__title">GrabShare Launches New Enhanced Option for Better Matched Rides</h3>
+                <p class="article-block__author">Grab Holdings Inc.</p>
+              </article>
+            </a>
+          </li>
+          <li class="featured__list-item">
+            <a class="featured__list-item-link" href="#">
+              <article class="article-block">
+                <div class="article-block__top">
+                  <span class="article-block__category">Apps</span>
+                  <div class="article-block__image" style="background-image: url('https://placeimg.com/350/219/nature');"></div>
+                </div>
+                <time class="article-block__date" datetime="07-11-19">JUL 11, 2019</time>
+                <h3 class="article-block__title">GrabShare Launches New Enhanced Option for Better Matched Rides</h3>
+                <p class="article-block__author">Grab Holdings Inc.</p>
+              </article>
+            </a>
+          </li>
+          <li class="featured__list-item">
+            <a class="featured__list-item-link" href="#">
+              <article class="article-block">
+                <div class="article-block__top">
+                  <span class="article-block__category">Apps</span>
+                  <div class="article-block__image" style="background-image: url('https://placeimg.com/350/219/nature');"></div>
+                </div>
+                <time class="article-block__date" datetime="07-11-19">JUL 11, 2019</time>
+                <h3 class="article-block__title">GrabShare Launches New Enhanced Option for Better Matched Rides</h3>
+                <p class="article-block__author">Grab Holdings Inc.</p>
+              </article>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <aside class="l-index__sidebar sidebar">SIDEBAR HERE</aside>
+  </div>
+</main>
 
 <?php
 get_footer();
---
+++ wp/wp-content/themes/prstation/template-parts/header/main.php
@@ -0,0 +1,36 @@
+<header class="header">
+  <div class="header__top">
+    <p class="header__top-left">Press Release, News Release Distribution Service in the Philippines</p>
+    <div class="header__top-right">
+      <a class="header__top-link" href="#">Post your PR</a>
+    </div>
+  </div>
+  <nav class="header__bottom nav">
+    <ul class="nav__list">
+      <li class="nav__list-item">
+        <a class="nav__list-item-link" href="#">TOP</a>
+      </li>
+      <li class="nav__list-item">
+        <a class="nav__list-item-link" href="#">Technology</a>
+      </li>
+      <li class="nav__list-item">
+        <a class="nav__list-item-link" href="#">App</a>
+      </li>
+      <li class="nav__list-item">
+        <a class="nav__list-item-link" href="#">Business</a>
+      </li>
+      <li class="nav__list-item">
+        <a class="nav__list-item-link" href="#">Event</a>
+      </li>
+      <li class="nav__list-item">
+        <a class="nav__list-item-link" href="#">Fashion</a>
+      </li>
+      <li class="nav__list-item">
+        <a class="nav__list-item-link" href="#">Restaurant</a>
+      </li>
+      <li class="nav__list-item">
+        <a class="nav__list-item-link" href="#">Blogger</a>
+      </li>
+    </ul>
+  </nav> 
+</header>
