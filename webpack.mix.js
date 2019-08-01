92082675039f2ede8d3740b0d64e3e951aefc1dc
Josh Dab <josiah.dabuet@gmail.com>
2019-08-01 17:12:00 +0800

    Added font files

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,9 @@ const { mix } = require('laravel-mix');
 
 const SRC_JS = 'src/js',
       SRC_CSS = 'src/sass',
+      SRC_FONTS = 'src/fonts',
       ASSETS_FOLDER = './wp/wp-content/themes/prstation/assets';
 
 mix.js(`${SRC_JS}/app.js`, ASSETS_FOLDER)
-   .sass(`${SRC_CSS}/style.scss`, ASSETS_FOLDER);
+   .sass(`${SRC_CSS}/style.scss`, ASSETS_FOLDER)
+   .copyDirectory(SRC_FONTS, `${ASSETS_FOLDER}/fonts`);
