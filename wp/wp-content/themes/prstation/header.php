6c51834b978fffe4352dbb60fb786791e1bb1300
Josh Dab <josiah.dabuet@gmail.com>
2019-08-26 15:53:36 +0800

    Added Google Analytics tags

--- wp/wp-content/themes/prstation/header.php
+++ wp/wp-content/themes/prstation/header.php
@@ -15,8 +15,19 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="google-site-verification" content="f4Jee3FCHBzim2xo-2PyPm5Qh1gnL8I8tPm91pv2Jyk" />
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/style.css">
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-145816016-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-145816016-1');
+  </script>
 
 	<?php wp_head(); ?>
 </head>
