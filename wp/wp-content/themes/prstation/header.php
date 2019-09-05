4f1444fb06aa577ee97dbecc53545a1cb0a409c4
Josh Dab <josiah.dabuet@gmail.com>
2019-09-05 20:30:42 +0800

    OGP and favicon added

--- wp/wp-content/themes/prstation/header.php
+++ wp/wp-content/themes/prstation/header.php
@@ -13,8 +13,25 @@
 ?><!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <title><?php wp_title('|'); ?></title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <meta name="format-detection" content="telephone=no">
+
+  <?php if(!is_single() || is_archive()): ?>
+    <meta property="og:image" content="<?php echo IMAGE_URI; ?>ogp.jpg">
+  <?php elseif(is_single() && !get_the_post_thumbnail()): ?>
+    <meta property="og:image" content="<?php echo IMAGE_URI; ?>ogp.jpg">
+  <?php endif; ?>
+  <?php if(is_single() && !$post->post_content): ?>
+    <meta name="description" content="<?php the_title(); ?>"/>
+    <meta name="og:description" content="<?php the_title(); ?>"/>
+  <?php endif; ?>
+
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.ico">
   <meta name="google-site-verification" content="NaeCw3sqN-uixuIqV3s9iK8OUFNVgqF6R7BEdKXRROc" />
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/style.css">
