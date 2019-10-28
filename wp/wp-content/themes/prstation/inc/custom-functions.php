2608eaa451738c7199ea08a266dc0dd9237174e1
Josh Dabuet <josiah@lightmedia.com.au>
2019-10-28 14:29:48 +0800

    User email not required on create/modify user

--- wp/wp-content/themes/prstation/inc/custom-functions.php
+++ wp/wp-content/themes/prstation/inc/custom-functions.php
@@ -44,3 +44,27 @@ function loader_enqueue() {
     wp_enqueue_script('loaderjs', ITEM_URL.'assets/index.js');
   }
 }
+
+// This will suppress empty email errors when submitting the user form
+add_action('user_profile_update_errors', 'my_user_profile_update_errors', 10, 3 );
+function my_user_profile_update_errors($errors, $update, $user) {
+    $errors->remove('empty_email');
+}
+
+// This will remove javascript required validation for email input
+// It will also remove the '(required)' text in the label
+// Works for new user, user profile and edit user forms
+add_action('user_new_form', 'my_user_new_form', 10, 1);
+add_action('show_user_profile', 'my_user_new_form', 10, 1);
+add_action('edit_user_profile', 'my_user_new_form', 10, 1);
+function my_user_new_form($form_type) {
+    ?>
+    <script type="text/javascript">
+        jQuery('#email').closest('tr').removeClass('form-required').find('.description').remove();
+        // Uncheck send new user email option by default
+        <?php if (isset($form_type) && $form_type === 'add-new-user') : ?>
+            jQuery('#send_user_notification').removeAttr('checked');
+        <?php endif; ?>
+    </script>
+    <?php
+}
